84e38b0e6d52cf5b4951d7e811ee329385599dba
carlcarl <carlcarlking@gmail.com>
2013-08-07 01:33:16 +0800

    1. Fix static file serving error 2. Change naming

--- web.js
+++ web.js
@@ -6,6 +6,8 @@ app.get('/', function(request, response) {
   var fs = require('fs');
   response.send(fs.readFileSync('index.html').toString());
 });
+process.env.PWD = process.cwd();
+app.use('/static', express.static(process.env.PWD + '/static'));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
